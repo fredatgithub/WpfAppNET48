28bf79cc810c785e8aea3ec197e280bef3a02a9e
Freddy Juhel <ext.freddy.juhel@reseau.sncf.fr>
2025-10-16 13:37:41 +0200

    clean code

--- WpfAppNET48/Properties/AssemblyInfo.cs
+++ WpfAppNET48/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -12,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfAppNET48")]
-[assembly: AssemblyCopyright("Copyright © MIT Freddy juhel 2025")]
+[assembly: AssemblyCopyright("Copyright © MIT Freddy Juhel 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
