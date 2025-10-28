ebb7b146b882ef748230b3df6959eadb253f5fa3
Freddy Juhel <ext.freddy.juhel@reseau.sncf.fr>
2025-10-28 17:03:17 +0100

    add code

---
+++ WindowsFormsAppSigned/FormMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsAppSigned
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(90, 90);
+      this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(165, 24);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Application signée";
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(376, 203);
+      this.Controls.Add(this.label1);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+      this.Name = "FormMain";
+      this.Text = "application signée";
+      this.Load += new System.EventHandler(this.FormMain_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ WindowsFormsAppSigned/FormMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppSigned
+{
+  public partial class FormMain: Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
